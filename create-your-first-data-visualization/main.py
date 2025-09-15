69a3e61466b1696c57424b7fb85e891c8b48268e
anna_b <nelson_anna9@hotmail.com>
2025-09-15 12:36:25 -0500

    Initial commit

---
+++ create-your-first-data-visualization/main.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt;
+
+snack_scores = [80, 60, 40]
+slice_labels = ["Reese's", "Fritos", "pistachios"]
+colors = ["orange", "yellow", "green"]
+
+plt.pie(snack_scores, labels=slice_labels, colors=colors)
+plt.title("My favorite snacks")
+plt.savefig("fav-snacks.png")
