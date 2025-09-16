bdfeed1eaa4562d283db9250c0d0ff1f89131f69
anna_b <nelson_anna9@hotmail.com>
2025-09-16 12:40:24 -0500

    Combine data with numpy

--- combining-data-with-numpy/main.py
+++ combining-data-with-numpy/main.py
@@ -1,13 +1,19 @@
 import csv
 import numpy as np
 
-# ADD CODE: Convert titanic1.csv
+with  open("titanic1.csv", "r") as file:
+    data = csv.reader(file)
+    header = next(data)
+    data_list = list(data)
+    titanic_data1 = np.array(data_list)
 
+with open("titanic2.csv", "r") as file:
+    data = csv.reader(file)
+    header = next(data)
+    data_list = list(data)
+    titanic_data2 = np.array(data_list)
 
-# ADD CODE: Convert titanic2.csv 
+combined_data = np.concatenate((titanic_data1, titanic_data2), axis=0)
 
-
-# ADD CODE: Merge two datasets
-
-
-# ADD CODE: Print out shape and number of dimensions of merged dataset
+print(combined_data.shape)
+print(combined_data.ndim)
