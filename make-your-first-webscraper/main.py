02adc9a38388566a80286126995e6a58b4f9a0a2
anna_b <nelson_anna9@hotmail.com>
2025-09-18 10:02:08 -0500

    Scrape website data

--- make-your-first-webscraper/main.py
+++ make-your-first-webscraper/main.py
@@ -1,7 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-honey_badger = requests.get("https://en.wikipedia.org/wiki/Honey_badger")
-honey_badger.raise_for_status()
-honey_badger_html = honey_badger.text.encode("utf-8")
-honey_badger_soup = BeautifulSoup(honey_badger_html, 'html.parser')
+# Set headers to mimic a real browser
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                  "(KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+}
+url = "https://en.wikipedia.org/wiki/Honey_badger#"
+# Send the request with headers
+response = requests.get(url, headers=headers)
+# Raise if there is an error
+response.raise_for_status()
+# html = resonse.text.encode("utf-8") # This code in unnecessary - Beautiful Soup can handle raw text directly
+soup = BeautifulSoup(response.text, 'html.parser')
+
+h2_tags = soup.find_all("h2")
+print(len(h2_tags))
+for element in h2_tags:
+    print(element)
+
+link_tags = soup.find_all("a")
+print(link_tags[:5])
+
+image_tags = soup.find_all("img")
+print(image_tags[-4:])
