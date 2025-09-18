d350fdc790668352db4847946005a1ba10125399
anna_b <nelson_anna9@hotmail.com>
2025-09-18 14:04:01 -0500

    Combine web scrapers

--- combining-web-scrapers/main.py
+++ combining-web-scrapers/main.py
@@ -2,7 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_soup(url):
-  r = requests.get(url)
+  headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                  "(KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+  }
+  r = requests.get(url, headers=headers)
   r.raise_for_status()
   html = r.text.encode("utf-8")
   soup = BeautifulSoup(html, "html.parser")
@@ -27,12 +31,29 @@ def get_animal(url):
   for row in rows:
     if "Class:" in row.get_text():
       animal_class = row.find("a").contents[0]
-  return animal_class
+      return animal_class
 
 
 category_data = get_categories("https://skillcrush.github.io/web-scraping-endangered-species/")
 
+# print(category_data)
 
-animal_class = get_animal("https://en.wikipedia.org/wiki/Honey_badger")
+collected_data = []
 
-print(animal_class)
+# animal_class = get_animal("https://en.wikipedia.org/wiki/Honey_badger")
+
+# print(animal_class)
+
+for category in category_data:
+  for animal in category_data[category]:
+    animal_href = animal["href"]
+    animal_name = animal.contents[0]
+    animal_class = get_animal(animal_href)
+    data = {
+      "Category": category,
+      "Name": animal_name,
+      "Class": animal_class
+    }
+    collected_data.append(data)
+
+print(collected_data)
