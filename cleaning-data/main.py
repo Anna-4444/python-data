3c19cc79ac164a0b2ecf76a94220df8be3c7df26
anna_b <nelson_anna9@hotmail.com>
2025-09-22 18:43:47 -0500

    Clean category data

--- cleaning-data/main.py
+++ cleaning-data/main.py
@@ -1,1 +1,11 @@
 import json
+import re
+
+with open("data.json", "r") as file:
+    data = json.load(file)
+
+for item in data:
+    item["Category"] = re.split(r"[ .(]", item["Category"])[0]
+    # print item["Category"]
+
+print(data)
