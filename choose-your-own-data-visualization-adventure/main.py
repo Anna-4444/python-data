ff4605c52967ac7511654bbcbeb5e00695869ac9
anna_b <nelson_anna9@hotmail.com>
2025-09-23 15:01:36 -0500

    Create scatterplot for book data

--- choose-your-own-data-visualization-adventure/main.py
+++ choose-your-own-data-visualization-adventure/main.py
@@ -2,7 +2,24 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-with open("books.csv","r") as datafile:
-    data = pd.read_csv(datafile,delimiter=",")
+df = pd.read_csv("books.csv")
+# print(df.columns)
 
-print(data["language_code"])
+def remove_outliers_iqr(dataframe, column):
+    Q1 = dataframe[column].quantile(0.25)
+    Q3 = dataframe[column].quantile(0.75)
+    IQR = Q3 - Q1
+    lower = Q1 - 1.5 * IQR
+    upper = Q3 + 1.5 * IQR
+    return dataframe[(dataframe[column] >= lower) & (dataframe[column] <= upper)]
+
+df_clean = remove_outliers_iqr(df, "# num_pages")
+df_clean = remove_outliers_iqr(df_clean, "ratings_count")
+
+sns.scatterplot(x="average_rating", y="# num_pages", hue="ratings_count", data=df_clean)
+plt.xlabel("Average Rating")
+plt.ylabel("Number of Pages")
+plt.title("Book Length and Average Rating")
+plt.legend(title="Number of Ratings")
+plt.savefig("book-length-rating.png")
+
